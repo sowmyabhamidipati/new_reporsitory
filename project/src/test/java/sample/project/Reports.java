80f4f967d7013c5472a0365a1798b4399b5a59e2
ganesh 3pura <ganesh 3pura@DESKTOP-A8RPO44.Dlink>
2021-08-30 16:47:25 +0530

    commits

--- project/src/test/java/sample/project/Reports.java
+++ project/src/test/java/sample/project/Reports.java
@@ -5,7 +5,7 @@ public class Reports {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hellooooo");
+		System.out.println("Helloo");
 	}
 
 }
